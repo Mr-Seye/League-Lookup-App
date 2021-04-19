b500703b1c471b4dee5943413d753f3548005ee6
FATIRL <jordanogunseye@gmail.com>
2021-04-19 16:04:41 +0100

    Project Upload
    moved the files for the project into the repo folder

---
+++ riotAPI/riotAPI.py
@@ -0,0 +1,90 @@
+import requests
+import riotAPIConsts as Consts
+
+#Import of downloaded module 'requests' and my custom module riotAPIConstants
+#RiotAPI class
+
+class RiotAPI(object): 
+
+
+
+    def __init__(self, api_key, region=Consts.REGIONS['europe_west']):
+        self.api_key = api_key
+        self.region = region
+
+        
+    def _request(self, api_url, params={}):
+            args = {'api_key': self.api_key}
+            for key, value in params.items():
+                if key not in args:
+                    args[key]=value
+
+            response = requests.get(
+                Consts.URL['base'].format(
+                    proxy=self.region,
+                    region=self.region,
+                    url=api_url
+                    ),
+                params=args
+                )
+            #print(response.url)
+            #print(response.json)
+            return response.json()
+            
+    def get_summoner_by_name(self, name):
+        api_url = Consts.URL['summoner_by_name'].format(
+             version=Consts.API_VERSIONS['summoner'],
+             names=name
+             )
+        return self._request(api_url)
+    
+    def get_total_champion_mastery(self, summonerId):
+        api_url = Consts.URL['total_champion_mastery'].format(
+            version=Consts.API_VERSIONS['champion-mastery'],
+            summonerId=summonerId
+        )
+        return self._request(api_url)
+
+    def get_league_entries_summoner(self, summonerId):
+        api_url = Consts.URL['match_entries'].format(
+            version=Consts.API_VERSIONS['league'],
+            summonerId=summonerId
+        )
+        return self._request(api_url)
+
+
+
+
+
+#class ddragon(object):
+    #def __init__(self, patch=Consts.DDRAGON_PATCH['current_patch'], language=Consts.DDRAGON_LANGUAGE['english_uk']):
+        #self.patch = patch
+        #self.language = language
+
+    #def _ddragon(self, ddragon_url, patch, language):
+        #ddresponse = requests.get(
+          #  Consts.URL['ddragon'].format(
+           #     patch=self.patch,
+            #    language=self.language,
+             #   url=ddragon_url
+          #  )
+#        )
+ #       print(ddresponse.json)
+  #      return ddresponse.json()
+     #       #print(response.url)
+   #         #print(response.json)
+        
+    #def get_champion_info(self, ddragon_url):
+      #  ddragon_url = Consts.URL['champion_info'].format(
+       #     patch=Consts.DDRAGON_PATCH['current_patch'],
+        #    language=Consts.DDRAGON_LANGUAGE['english_uk']
+      #  )
+        #return self._ddragon
+
+    #def new(self, name):
+        #api_url = Consts.URL['summoner_by_name'].format(
+             #version=Consts.API_VERSIONS['summoner'],
+             #names=name
+             #)
+
+        
---
+++ riotAPI/riotAPIConsts.py
@@ -0,0 +1,70 @@
+URL = {
+    'base': 'https://{proxy}.api.riotgames.com/lol/{url}',
+    'total_mastery_points_by_champion': 'champion-mastery/v{version}/champion-masteries/by-summoner/{summonerId}',
+    'champion_mastery': 'champion-mastery/v{version}/champion-masteries/by-summoner/{summonerId}/by-champion/{championId}',
+    'total_champion_mastery': 'champion-mastery/v{version}/scores/by-summoner/{summonerId}',
+    'champion_rotations': 'platform/v{version}/champion-rotations',
+    'apex_queues': 'league-exp/v{version}/entries/{queue}/{tier}/{division}',
+    'challenger_league': 'league/v{version}/challengerleagues/by-leagues/{queue}',
+    'grand_master_league': 'league/v{version}/grandmasterleagues/by-leagues/{queue}',
+    'master_league': 'league/v{version}/masterleagues/by-leagues/{queue}',
+    'match_entries': 'league/v{version}/entries/by-summoner/{summonerId}',
+    'league_status': 'status/v{version}/shard-data',
+    'match': 'match/v{version}/matches/{matchId}',
+    'account_match_list': 'match/v{version}/matchlists/by-account/{accountId}',
+    'match_timeline': 'match/v{version}/timelines/by-match/{matchId}',
+    'spectate_match_info': 'spectator/v{version}/active-games/by-summoner/{summonerId}',
+    'summoner_by_name': 'summoner/v{version}/summoners/by-name/{names}',
+    'third_party_code': 'platform/v{version}/third-party-code/by-summoner/{summonerId}',
+    'ddragon': 'https://ddragon.leagueoflegends.com/cd/{patch}/data/{language}/',
+    'champion_info': 'champion.json'
+}
+#This is the base URL for any API address on the Riot API, it uses HTTP Secure, followed by a proxy in this case "EUW1", then the api link, then /lol which specifies that it is referring to League of Legends then {url} which is derived from riotAPI
+#Essentially a module which referres to summoner information based on summoner name
+
+API_VERSIONS = { 
+    'champion-mastery': '4',
+    'champion': '3',
+    'league-exp': '4',
+    'league': '4',
+    'lol-status': '3',
+    'match': '4',
+    'spectator': '4',
+    'summoner': '4',
+    'third-party-code': '4'
+    
+}
+
+DDRAGON_PATCH = {
+    'current_patch': '9.24.2'
+}
+
+DDRAGON_LANGUAGE = {
+    'english_uk': 'en_GB'
+}
+
+#This is a list of the appropriate module versions
+
+REGIONS = { 
+    'brazil': 'br1',
+    'europe_nordic_east': 'eun1',
+    'europe_west': 'euw1',
+    'japan': 'jp1',
+    'korea': 'kr',
+    'latin_america_north': 'la1',
+    'latin_america_south': 'la2',
+    'north_america': 'na1',
+    'oceana': 'oc1',
+    'turkey': 'tr1',
+    'russia': 'ru1'
+
+}
+
+#The codes for the modules being used
+
+PROXIES = { 
+    'europe_west': 'euw1',
+    'north_america': 'na1'
+}
+
+#This is not required as of now
